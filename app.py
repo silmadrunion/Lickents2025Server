fa18120e644cc8fb5ca6544511d2362308265cfb
Mihai-Codrin Robota <crobota99@gmail.com>
2025-06-12 15:25:16 +0300

    CORS fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 #from flask_cors import CORS
 
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 import json
@@ -37,6 +38,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 @app.get("/")
 async def root():
