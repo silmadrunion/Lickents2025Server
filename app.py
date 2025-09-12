2e3cedf5ef83a799676cc865a5b13d5bcb9a5623
Mihai-Codrin Robota <crobota99@gmail.com>
2025-09-13 02:30:45 +0300

    Minor Cleanup

--- app.py
+++ app.py
@@ -60,52 +60,5 @@ app.include_router(listing.router)
 async def root():
     return "This is the server root"
 
-'''
-@app.get("/game", status_code=status.HTTP_200_OK)
-async def get_all_games(user_id: Union[str, None] = None):
-    if user_id:
-        print("User Query Parameter provided: ", user_id)
-        gameObj = await handler.get_multiple_from_collection({"gameOwnerId": user_id}) #Recheck sometime
-    else:
-        gameObj = await handler.get_multiple_from_collection()
-    print(gameObj)
-    return gameObj
-
-@app.get("/game/{game_id}", status_code=status.HTTP_200_OK)
-async def get_game_by_id(game_id):
-    print(game_id)
-    gameObj = await handler.get_from_collection(game_id)
-    print(gameObj)
-    return gameObj
-
-@app.post("/game", status_code=status.HTTP_201_CREATED)
-async def post_one_game(game: GameDetails):
-    result = await game.insert()
-    return result
-
-@app.put("/game", status_code=status.HTTP_201_CREATED)
-async def put_one_game(game: GameDetails):
-    result = await game.replace()
-    return result
-
-@app.patch("/game", status_code=status.HTTP_201_CREATED)
-async def patch_one_game(game: GameDetails):
-   document_dict = {}
-   for key, value in game.model_dump().items():
-       if value is not None:
-           document_dict[key] = value
-   result = await game.update({'$set': document_dict})
-
-@app.delete("/game/{game_id}", status_code=status.HTTP_200_OK)
-async def delete_one_game(game_id):
-    game = await GameDetails.get(game_id)
-    print(game)
-    await game.delete()
-    return {}
-'''
-
-'''
-
-'''
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
